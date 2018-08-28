2c14b403d088161910744edc5d3f07cd06b1bbbc
JialiuWuxi <37652140+JialiuWuxi@users.noreply.github.com>
2018-08-28 14:30:02 +0800

    add post client api

--- routes/api.js
+++ routes/api.js
@@ -102,13 +102,32 @@ router.get('/clients', function(req, res, next) {
         getListItems(client, process.env.SITE_NAME, process.env.CLIENT_LIST_NAME)
         .then(result => res.status('200').send(result))
         .catch(err => res.status(err.statusCode).send(err));
+    }else{
+        res.status('401').send('empty token');
+    }
+});
 
+router.post('/clients', function(req, res, next) {
+
+    const authToken = getTokenFromHeader(req);
+    const clientInfor = req.body;
+
+    if(authToken){
+        const client = graph.Client.init({
+            authProvider: (done) => {
+              done(null, authToken);
+            }
+        });
+
+
+        //需要添加重复创建判断
+        saveListItems(client, process.env.SITE_NAME, process.env.CLIENT_LIST_NAME, clientInfor)
+        .then(result => res.status('200').send(result))
+        .catch(err => res.status(err.statusCode || '500').send(err));
 
     }else{
         res.status('401').send('empty token');
     }
-
-
 });
 
 
@@ -164,5 +183,39 @@ function getTokenFromHeader(req) {
         return undefined;
     }
 }
+
+async function saveListItems(client, siteName, listName, data) {
+    try{
+        //Get root name
+        const rootName = (await client
+        .api(`/sites/root`)
+        .get()).siteCollection.hostname;
+
+        //Get the Site ID
+        const siteID = (await client
+        .api(`/sites/${rootName}:/sites/${siteName}`)
+        .select('id')
+        .get()).id
+        
+        const listID = (await client
+        .api(`/sites/${siteID}/lists`)
+        .filter(`displayName eq '${listName}'`)
+        .get()).value[0].id
+        
+
+        let newItems = await client
+        .api(`/sites/${siteID}/lists/${listID}/items`)
+        .post({
+            "fields": data
+            }
+        )
+
+        return newItems;
+        
+    }catch(err){
+        throw err;
+    };
+}
+
 
 module.exports = router;
